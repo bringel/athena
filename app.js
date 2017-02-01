1ffd0e50107e985507113576eeb7a5a242d7d5c9
Brad Ringel <brad.ringel@gmail.com>
2017-01-31 21:18:24 -0500

    Get api token from twitter on startup

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const querystring = require('querystring');
+const request = require('request');
+require('dotenv').config();
 
-var app = express();
+let app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 4200));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,4 +19,29 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+  authorizeTwitter(process.env.twitterAPIKey, process.env.twitterAPISecret);
 });
+
+function authorizeTwitter(appKey, appSecret) {
+  let credentials = `${querystring.escape(appKey)}:${querystring.escape(appSecret)}`;
+  let buf = new Buffer(credentials);
+  let encodedCredentials = buf.toString('base64');
+
+  let options = {
+    headers: {
+      'Authorization': `Basic ${encodedCredentials}`,
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    },
+    body: 'grant_type=client_credentials'
+  };
+
+  request.post('https://api.twitter.com/oauth2/token', options, function(error, response, body) {
+    if (!error && response.statusCode == 200){
+      process.env.twitterAPIToken = body.access_token;
+    }
+  });
+}
+
+function isTwitterAuthorized() {
+  return process.env.twitterAPIToken !== null && process.env.twitterAPIToken !== undefined;
+}
